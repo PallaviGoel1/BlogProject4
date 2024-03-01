fd3483bf77ba1e8fd3931f05707b6753adba1591
Pallavi Goel <pallavigoel2610@gmail.com>
2024-03-01 09:45:38 +0000

    add env.py file

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://nvngilch:Irv1hoGEUEDfe3IUzm6AD3y1slfbAOme@flora.db.elephantsql.com/nvngilch"
+os.environ["SECRET_KEY"] = "f%&pe@u5bs3p1)+*4v^9+o^54@pwy%p)ek$o42(7h#($l_=o%2"
+os.environ["CLOUDINARY_URL"]= "cloudinary://172374751467919:kkF5I3hQYkE965tCNPkDADPXwkk@dyrvatemi"
