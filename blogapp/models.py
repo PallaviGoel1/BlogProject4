fc64a64b1f7dfbeef6c823771f342cb61618f66b
Pallavi Goel <pallavigoel2610@gmail.com>
2024-03-13 21:05:09 +0000

    try to deploy

--- blogapp/models.py
+++ blogapp/models.py
@@ -22,11 +22,11 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     date = models.DateTimeField(default=timezone.now)
     category =  models.CharField(max_length=255, default='coding')
-    body = models.TextField()
+    body = models.TextField(null= True, blank= True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User,related_name = 'blogpost_like', blank=True)
-    featured_image = CloudinaryField('image', default='placeholder')
+    #featured_image = CloudinaryField('image', default='placeholder')
     
     class Meta:
         ordering = ['-date']
